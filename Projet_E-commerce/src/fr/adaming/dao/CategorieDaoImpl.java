dc310e12140d583578907911663c611ce5427495
Adaming <Adaming@192.168.1.168>
2018-01-08 15:28:32 +0000

    Anais:maj des class

---
+++ Projet_E-commerce/src/fr/adaming/dao/CategorieDaoImpl.java
@@ -0,0 +1,25 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Categorie;
+
+public class CategorieDaoImpl implements ICategorieDao{
+
+	@Override
+	public List<Categorie> getAllCategorie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Categorie> getCategorieById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+
+}
